416238e64ccbd4adf4234227d0ed8ee3d6dd25d5
roshan1727 <rakeshroshanp27@gmail.com>
2024-03-18 11:03:03 +0530

    update

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -62,11 +62,59 @@ exports.editProduct = async (req, res) => {
     } catch (error) {
         console.error("Error executing query: ", error.message);
     }
-    res.render("home", {
+    res.render("edProduct", {
         rows
     });
-    res.render("edProduct");
+    // res.render("edProduct");
 }
-// exports.deleteProduct = async (req, res) => {
-//     res.render("deletemsg");
-// }
+
+
+// save and update the record
+exports.edit = async (req, res) => {
+    let rows, fields; // Define rows and fields variables here
+    let id = req.params.id;
+    const {
+        name,
+        Price,
+        Location
+    } = req.body;
+    try {
+        const connection = await pool.getConnection();
+        [rows, fields] = await connection.query("update product SET NAME=?, price=?, location=? WHERE ID=?", [name, Price, Location, id]);
+        connection.release();
+        try {
+            const connection = await pool.getConnection();
+            [rows, fields] = await connection.query("SELECT * FROM product where id=?", [id]);
+            connection.release();
+            console.log("Database query successful");
+        } catch (error) {
+            console.error("Error executing query: ", error.message);
+        }
+        res.render("edProduct", {
+            rows,
+            msg: "User Details Updated Successfully"
+        });
+    } catch (error) {
+        console.error("Error executing query: ", error.message);
+        res.render("edProduct", {
+            msg: "Error updating user details"
+        });
+    }
+}
+
+
+exports.delete = async (req, res) => {
+    try {
+        const connection = await pool.getConnection();
+        let id = req.params.id;
+        await connection.query("DELETE FROM product WHERE ID=?", [id]);
+        connection.release();
+        console.log("Product deleted successfully");
+        res.redirect("/");
+    } catch (error) {
+        console.error("Error deleting product: ", error.message);
+        res.render("home", {
+            msg: "Error deleting product"
+        });
+    }
+}
--- server/routes/product.js
+++ server/routes/product.js
@@ -7,5 +7,12 @@ router.get("/addProduct", productController.addproduct)
 router.post("/addProduct", productController.saveRecord);
 // update records
 router.get("/editproduct/:id", productController.editProduct);
+router.post("/editproduct/:id", productController.edit);
+
+
+// delete records
+router.get("/deleteproduct/:id", productController.delete);
+
+
 
 module.exports = router;
